995dcbd7f133ec15409a8115f560ce3817f6554a
Christopher Pho <pho3818919@gmail.com>
2022-02-24 14:52:34 -0500

    Start roadrunner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpModeTemplate.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystem.Carousel;
+import org.firstinspires.ftc.teamcode.subsystem.Deposit;
+import org.firstinspires.ftc.teamcode.subsystem.Intake;
+
+abstract public class OpModeTemplate extends CommandOpMode {
+    protected SampleMecanumDrive mecanumDrive;
+    protected Deposit deposit;
+    protected Intake intake;
+    protected Carousel carousel;
+
+    protected GamepadEx driveGamepad;
+    protected GamepadEx opGamepad;
+
+    protected void initHardware(boolean resetEncoders) {
+        mecanumDrive = new SampleMecanumDrive(hardwareMap);
+
+        deposit = new Deposit(hardwareMap);
+        intake = new Intake(hardwareMap);
+        carousel = new Carousel(hardwareMap);
+
+        register(deposit, intake, carousel);
+
+        driveGamepad = new GamepadEx(gamepad1);
+        opGamepad = new GamepadEx(gamepad2);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Deposit.java
@@ -10,5 +10,37 @@ public class Deposit extends SubsystemBase {
     private DcMotorEx liftMotor;
     private ServoEx stopper;
 
+    private double power;
 
+    public Deposit(HardwareMap hwMap) {
+        this.hwMap = hwMap;
+
+        liftMotor = hwMap.get(DcMotorEx.class, "liftMotor");
+    }
+
+    @Override
+    public void register() {
+        super.register();
+    }
+
+    @Override
+    public void periodic() {
+        liftMotor.setPower(power);
+    }
+
+    public void extend() {
+        power = 1.0;
+    }
+
+    public void retract() {
+        power = -1.0;
+    }
+
+    public void stop() {
+        power = 0.0;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
 }
